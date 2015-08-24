d6090221fa2a4ccc5f5c9cf1e8b5787335b4a31a
Felix Braun <hazzl@falix.de>
2015-08-24 18:55:09 +0200

    implement configuration handling and blank pointer conf

---
+++ config.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+class configHandler():
+	def __init__(self,conn):
+		self._conn = conn
+		self._config = {}
+		q = self._conn.cursor()
+		q.execute("SELECT * FROM config")
+		for (key, val) in q.fetchall():
+			print (key, "=>", val)
+			self._config[key]=val
+	def get(self, key):
+		if key in self._config.keys():
+			return self._config[key]
+		else:
+			return None
+	def set(self, key, val):
+		q = self._conn.cursor()
+		q.execute("INSERT OR REPLACE INTO config VALUES (?,?)",
+				(key,val))
+		self._config[key]=val
+		self._conn.commit()
